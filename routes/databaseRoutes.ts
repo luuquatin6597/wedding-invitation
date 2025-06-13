7b9c718c99f69d93fb96abc9ce43667bb1f06780
Luu Quang Tinh <luuquangtinh6597@gmail.com>
2025-06-13 13:45:53 +0700

    remove file

--- routes/databaseRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { initializeDatabase } from '../controllers/databaseController';
-
-const router = express.Router();
-
-router.get('/connect', initializeDatabase);
-
-export default router;
