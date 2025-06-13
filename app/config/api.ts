219e30fc1e7ce339fa8b937a48e5fddb9948dff7
Luu Quang Tinh <luuquangtinh6597@gmail.com>
2025-06-13 14:18:19 +0700

    edit url

--- app/config/api.ts
+++ app/config/api.ts
@@ -1,6 +1,9 @@
 const SERVER_PORT = process.env.SERVER_PORT || 3001;
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || `http://localhost:${SERVER_PORT}/api`;
-
+export const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL && process.env.NODE_ENV === 'production'
+    ? process.env.NEXT_PUBLIC_API_BASE_URL
+    : `http://localhost:${SERVER_PORT}/api`;
+    
 export const API_ENDPOINTS = {
     users: `${API_BASE_URL}/users`,
     templates: `${API_BASE_URL}/templates`,
